29a839bce7578eef4ec4ef4b28807913afcc901d
jwu2018 <jyalu.wu@hotmail.com>
2020-02-02 16:19:33 -0500

    added makefile

---
+++ .cs3013_project2.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/lu/Documents/Project2/cs3013_project2.ko := ld -r -m elf_i386 -T ./scripts/module-common.lds --build-id  -o /home/lu/Documents/Project2/cs3013_project2.ko /home/lu/Documents/Project2/cs3013_project2.o /home/lu/Documents/Project2/cs3013_project2.mod.o
